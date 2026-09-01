333cda09dad1ce35b0cdc54167dd83842829a30a
Alex Schiszler das Chagas <92379105+Alex-Schiszler@users.noreply.github.com>
2026-09-01 11:52:15 -0300

    Estrutura oficial PWA com icone.png, manifest.json e sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,4 @@
-﻿self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request).catch(function() {
-    return new Response('Offline');
-  }));
+﻿self.addEventListener('fetch', function(event) {
+  // Mantém o comportamento padrão de busca na rede
+  event.respondWith(fetch(event.request));
 });
